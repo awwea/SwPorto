a7e7c2f195bac4100752fae32fbb1c2af4cd55e6
awwea <admin@awwea.com>
2017-06-19 02:58:36 +0000

    Correct color var = transparent

--- Model/Config/Backend/Design/Color/Validatetransparent.php
+++ Model/Config/Backend/Design/Color/Validatetransparent.php
@@ -7,7 +7,7 @@ class Validatetransparent extends \Magento\Framework\App\Config\Value
     public function beforeSave()
     {
         $v = $this->getValue();
-        if ($v == 'rgba(0, 0, 0, 0)')
+        if ($v == 'rgba(0, 0, 0, 0, 0)')
         {
             $this->setValue('transparent');
         }
